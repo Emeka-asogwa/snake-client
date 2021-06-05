53f0c848c9ccb720b0e32abd379b3121363aa783
Chukwuemeka (Emeka) Asogwa <menco23real@yahoo.com>
2021-06-05 18:33:59 +0000

    ✍🏽 Refactoring

---
+++ inputs.js
@@ -0,0 +1,32 @@
+const {UPKEY,DOWNKEY,LEFTKEY,RIGHTKEY} = require('./directions');
+// Stores the active TCP connection object.
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = key => {
+    if (key === '\u0003') {
+      process.exit();
+    } else if (key === UPKEY) {
+      conn.write('Move: up');
+    } else if (key === LEFTKEY) {
+      conn.write('Move: left');
+    } else if (key === DOWNKEY) {
+      conn.write('Move: down');
+    } else if (key === RIGHTKEY) {
+      conn.write('Move: right');
+    } 
+  };
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  });
+  return stdin;
+};
+
+
+module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,4 +1,18 @@
-const connect = require('./client');
+const { connect } = require('./client');
 
+const { setupInput } = require('./inputs');
 
+console.log("Connecting ...");
 connect();
+setupInput();
+
+//setupInput(connect());
+
+
+// const setupInput = function () {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding("utf8");
+//   stdin.resume();
+//   return stdin;
+// };
